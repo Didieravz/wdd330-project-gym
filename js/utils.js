d693ddef4499c1f5c0195dbab08933d9bd876682
didieravz <didiervz@hotmail.com>
2025-04-02 15:05:04 -0500

    Ajustes estructura proyecto

--- js/utils.js
+++ js/utils.js
@@ -9,7 +9,7 @@ async function loadTemplate(path) {
 export async function loadHeaderFooter() {
     // console.log("Probando...");
     if (!headerLoaded) {
-        const headerTemplate = await loadTemplate("/src/partials/header.html");
+        const headerTemplate = await loadTemplate("/partials/header.html");
         const headerElement = document.querySelector("#header");
         if (!headerElement.innerHTML.trim()) {
             headerElement.innerHTML = headerTemplate;
@@ -18,7 +18,7 @@ export async function loadHeaderFooter() {
     }
 
     if (!footerLoaded) {
-        const footerTemplate = await loadTemplate("/src/partials/footer.html");
+        const footerTemplate = await loadTemplate("/partials/footer.html");
         const footerElement = document.querySelector("#footer");
         if (!footerElement.innerHTML.trim()) {
             footerElement.innerHTML = footerTemplate;
