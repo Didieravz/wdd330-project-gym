1ba4a4256df5373ee86e8e7efd9292eb3a220bba
didieravz <didiervz@hotmail.com>
2025-04-14 20:03:10 -0500

    video and documents project

--- js/app.js
+++ js/app.js
@@ -11,18 +11,3 @@ document.addEventListener("DOMContentLoaded", () => {
     if (registerBtn) registerBtn.addEventListener("click", () => navigateTo("../views/register.html"));
     if (getDietBtn) getDietBtn.addEventListener("click", () => navigateTo("../views/diet.html"));
 });
-
-
-// Estructura del proyecto:
-// - index.html (Página principal)
-// - views/
-//    - gyms.html (Mapa y lista de gimnasios)
-//    - register.html (Formulario de inscripción)
-//    - diet.html (API de Edamam)
-// - js/
-//    - app.js (Carga dinámica y navegación)
-//    - map.js (Manejo del mapa y gimnasios)
-//    - register.js (Formulario de inscripción con Formspree)
-//    - diet.js (Manejo de la API de Edamam)
-//    - storage.js (Manejo de LocalStorage)
-//    - utils.js (Funciones auxiliares y carga de header/footer)
